6963f8a7d9a33a2a618e839ac3a64daf4e0cdaad
Paul Franklin <64500978+Frankers71@users.noreply.github.com>
2021-03-15 12:51:07 +0000

    Update main_Seb.py
    train_test_split is now in model_selection

--- main_Seb.py
+++ main_Seb.py
@@ -1,6 +1,6 @@
 import numpy as np 
 import pandas as pd
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier #imports the dtree classifier
 from sklearn.metrics import accuracy_score #using accuracy to rate the model
 from sklearn import tree
